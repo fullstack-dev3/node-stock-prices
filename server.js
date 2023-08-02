8f55b23da81fee1b0320b9b6ad308f7dcbc7c688
full-stack-david <davidchan12323@gmail.com>
2023-08-02 04:05:39 -0500

    Update request error message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', async (req, res) => {
     const { symbol } = req.query;
     
     if (!symbol) {
-      return res.status(400).json({ error: 'Symbol parameter is required. Ex: AAPL' });
+      return res.status(400).json({ error: 'symbol parameter is required. Ex: symbol=AAPL' });
     }
 
     const url = `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?interval=1d&range=1d`;
