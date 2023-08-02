45f342a85b37fd10d5199ae4f8e73bd2be0f813f
full-stack-david <davidchan12323@gmail.com>
2023-08-02 03:48:15 -0500

    Get stock prices data

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # node-stock-prices
+You can get the Stock Prices by using a simple API in Node.js.
+The prices data is grabbing from Yahoo Finance website.
+Example: http://localhost:3000/stock-price?symbol=AAPL
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/stock-price', async (req, res) => {
+  try {
+    const { symbol } = req.query;
+    
+    if (!symbol) {
+      return res.status(400).json({ error: 'Symbol parameter is required' });
+    }
+
+    const url = `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?interval=1d&range=1d`;
+
+    const response = await axios.get(url);
+    const data = response.data;
+
+    res.json({
+      symbol,
+      currentPrice: data.chart.result[0].meta.regularMarketPrice,
+      exchangeName: data.chart.result[0].meta.exchangeName,
+      high: data.chart.result[0].indicators.quote[0].high,
+      low: data.chart.result[0].indicators.quote[0].low,
+      open: data.chart.result[0].indicators.quote[0].open,
+      close: data.chart.result[0].indicators.quote[0].close
+    });
+  } catch (error) {
+    console.error('Error fetching stock price:', error.message);
+    res.status(500).json({ error: 'Failed to fetch stock price' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
